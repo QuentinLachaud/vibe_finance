ca318037343ea3e1db288aa416f3a6bdea469e97
Quentin Lachaud <quentin.lachaud@gmail.com>
2026-02-23 17:18:50 +0000

    fixing limit error

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { fileURLToPath } from 'url'
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
 
   resolve: {
