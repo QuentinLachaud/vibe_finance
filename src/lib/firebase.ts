9585741455c4e25e6cbb3adc626f7462588357b5
Quentin Lachaud <quentin.lachaud@gmail.com>
2026-02-23 12:15:12 +0000

    firebase fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCshexKWFewBv7NmjJUX3o6DLLc-47jX0Y",
-  authDomain: "takehomecalc-c86a7.firebaseapp.com",
-  projectId: "takehomecalc-c86a7",
-  storageBucket: "takehomecalc-c86a7.firebasestorage.app",
-  messagingSenderId: "350118392468",
-  appId: "1:350118392468:web:1186f627471bbb313eba81",
-  measurementId: "G-P5QDTJW14G",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
